dab8e0c5c43189c8119af56a486a0cf9008570dd
gretagg3 <ggiorgetti4@studenti.uninsubria.it>
2025-12-02 22:33:11 +0100

    aggiunta commenti classe Recensione

--- src/Recensione.java
+++ src/Recensione.java
@@ -1,14 +1,18 @@
 public class Recensione {
+
+    //campi
     private String testo;
     private int stelle;
     private String rispostaRecensione;
 
+    //costruttore
     public Recensione(String testo, int stelle, String rispostaRecensione){
         this.testo=testo;
         this.stelle=stelle;
         this.rispostaRecensione=rispostaRecensione;
     }
 
+    //metodi getter e setter
     public String getTesto() {
         return testo;
     }
